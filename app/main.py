b654d2bc17fc9f82219d46408b12e78666ca59b0
Oleksandr Voitovych <voytovicholeksandr@gmail.com>
2026-05-28 22:19:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,4 +16,21 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 
-# write your code here
+# ... твої 8 змінних, які вже дані в завданні ...
+
+sorted_variables = {
+    "mutable": [
+        my_favourite_films,
+        marks,
+        collection_of_coins
+    ],
+    "immutable": [
+        lucky_number,
+        pi,
+        one_is_a_prime_number,
+        name,
+        profile_info
+    ]
+}
+
+
